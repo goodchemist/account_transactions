ff410d1731ecdbcf290e59896cb08dee47592269
good_chemist <iolgasergeevna@yandex.ru>
2023-11-24 17:54:17 +0800

    fix test_get_database

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,9 +8,9 @@ from src.utils import (get_database, change_date_to_datetime_type, sort_database
 
 
 def test_get_database():
-    CURRENT_DIR = os.path.dirname(__file__)
-    DATA_FOR_TEST_PATH = os.path.join(CURRENT_DIR, 'data_for_test.json')
-    assert get_database(DATA_FOR_TEST_PATH) == [1, 2, 3, 4]
+    current_dir = os.path.dirname(__file__)
+    data_for_test_path = os.path.join(current_dir, 'data_for_test.json')
+    assert get_database(data_for_test_path) == [1, 2, 3, 4]
 
 
 @pytest.mark.parametrize('array, result', [
